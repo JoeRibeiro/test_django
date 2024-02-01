70d64887e605425a315b694f98dbd08edb2d5fac
Joseph Ribeiro (Cefas) <joseph.ribeiro@cefas.gov.uk>
2024-02-01 16:01:17 +0000

    Ran with no instances detected

--- maskRCNN.py
+++ maskRCNN.py
@@ -8,7 +8,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 # Root directory of the project
-ROOT_DIR = os.path.abspath("../") # C:/Users/JR13/OneDrive - CEFAS/My onedrive documents/test_django/maskrcnn/   frames output_masks
+ROOT_DIR = os.path.abspath("C:/Users/JR13/OneDrive - CEFAS/My onedrive documents/test_django/maskrcnn/") # C:/Users/JR13/OneDrive - CEFAS/My onedrive documents/test_django/maskrcnn/   frames output_masks
 
 
 # Import Mask RCNN
@@ -17,8 +17,8 @@ from mrcnn import utils
 import mrcnn.model as modellib
 from mrcnn import visualize
 # Import COCO config
-sys.path.append("C:/Users/JR13/OneDrive - CEFAS/My onedrive documents/test_django/maskrcnn/mask_rcnn_coco.h5")  # To find local version
-import coco
+sys.path.append("mask_rcnn_coco.h5")  # To find local version
+from pycocotools import coco
 
 #%matplotlib inline 
 
@@ -34,3 +34,67 @@ if not os.path.exists(COCO_MODEL_PATH):
 # Directory of images to run detection on
 IMAGE_DIR = os.path.join(ROOT_DIR, "images")
 
+
+from mrcnn.config import Config
+class CocoConfig(Config):
+    """Configuration for training on MS COCO.
+    Derives from the base Config class and overrides values specific
+    to the COCO dataset.
+    """
+    # Give the configuration a recognizable name
+    NAME = "coco"
+    # We use a GPU with 12GB memory, which can fit two images.
+    # Adjust down if you use a smaller GPU.
+    IMAGES_PER_GPU = 2
+    # Uncomment to train on 8 GPUs (default is 1)
+    # GPU_COUNT = 8
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 80  # COCO has 80 classes
+
+class InferenceConfig(CocoConfig):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+config = InferenceConfig()
+config.display()
+
+
+
+# Create model object in inference mode.
+model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+# Load weights trained on MS-COCO
+model.load_weights(COCO_MODEL_PATH, by_name=True)
+
+
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
+
+
+
+# # Load a random image from the images folder
+file_names = next(os.walk(IMAGE_DIR))[2]
+image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
+
+# Run detection
+results = model.detect([image], verbose=1)
+
+# Visualize results
+r = results[0]
+visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], 
+                            class_names, r['scores'])
--- readme.md
+++ readme.md
@@ -5,16 +5,18 @@ Save environment with
 conda env export > environment.yml
 
 
-Step-by-step build:
+Step-by-step build (I am doubtful a .yml will install properly):
 conda create --name djangoenv python=3.7 numpy=1.19.5 imutils=0.5.4
 conda activate djangoenv
 pip install django-labeller opencv-python
 # At this point, django labeller works
+# Now we want maskrcnn
 pip install -r "C:\Users\JR13\OneDrive - CEFAS\My onedrive documents\test_django\maskrcnn\Mask_RCNN\requirements.txt"
 cd "C:\Users\JR13\OneDrive - CEFAS\My onedrive documents\test_django\maskrcnn\Mask_RCNN\"
 python setup.py install
-pip install coco
-
+# For maskcsnn, Coco needs to be the windows version, the default is not supported and you might need to install visual studio build tools
+pip install pycocotools-windows
+pip install h5py==2.10.0
 
 .py file usage:
 python "C:\Users\JR13\OneDrive - CEFAS\My onedrive documents\test_django\dump_frames.py"
